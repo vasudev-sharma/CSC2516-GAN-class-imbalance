c4681e34c6710282b036299b15603f339d9cdd01
vasudev-sharma <sharmavasudev1998@gmail.com>
2022-03-21 20:14:24 +0000

    Update readme and bash scripts

--- bash_scripts/run_data.sh
+++ bash_scripts/run_data.sh
@@ -1,7 +1,9 @@
-!# /bin/bash
+#! /bin/bash
 
-cd data 
+cd data/covid-chestxray-dataset
 git clone https://github.com/ieee8023/covid-chestxray-dataset.git 
+mv covid-chestxray-dataset/* . & rm covid-chestxray-dataset
+cd ..
 
 
 curl 'https://storage.googleapis.com/kaggle-data-sets/17810/23812/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20220319%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20220319T215442Z&X-Goog-Expires=259199&X-Goog-SignedHeaders=host&X-Goog-Signature=853ab12073e1008961b421e269d76ed97545de3edd21097bc4046abb0f98cb62d166f8a1911f85a3b469433ee483ecd182eeee72312454e561bd13822cdfc32a6e2ab35b0a85f648942dfe372f63f1d0549d39e5f03ba5eb9abddb70be70e3865795ecc6fc047bf0c256712fbfc8cbce39a3b3ab6641bcc741ed4e36e5fb60650bc8e387fbe6b57546b7cc359b41c2d6e6a4b442a44ae97f4db79a2b064e16394b6d8bfca90d9814b6ec56da8d508c1e169e80da550412dd52e62c5e06c515d050f3326f4fc58dd47dbc0bcca8e5890691f72f9dfd4ff63c2373db27c01cde5e8db7ea6cebe2b58005879fb12be8b4e6bda290823402c139d919832d9d953014' \
@@ -19,9 +21,9 @@ curl 'https://storage.googleapis.com/kaggle-data-sets/17810/23812/bundle/archive
   -H 'referer: https://www.kaggle.com/' \
   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
   --compressed -o data.zip
-unzip data.zip -d RNSA_Pneumonia
+unzip data.zip -d RSNA_Pneumonia
 rm data.zip
 
-cd ../
+cd ..
 
 
