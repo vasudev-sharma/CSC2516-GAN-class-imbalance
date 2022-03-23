1277ca8191477e3a9d6f31cf6525017f6339b5f6
vasudev-sharma <sharmavasudev1998@gmail.com>
2022-03-23 05:39:44 +0000

    Fix borken bash file

--- bash_scripts/run_data.sh
+++ bash_scripts/run_data.sh
@@ -1,10 +1,13 @@
-!# /bin/bash
+#! /bin/bash
 
-cd data 
+cd data/covid-chestxray-dataset
 git clone https://github.com/ieee8023/covid-chestxray-dataset.git 
+mv covid-chestxray-dataset/* . & rm -r covid-chestxray-dataset
+cd ..
 
 
-curl 'https://storage.googleapis.com/kaggle-data-sets/17810/23812/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20220319%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20220319T215442Z&X-Goog-Expires=259199&X-Goog-SignedHeaders=host&X-Goog-Signature=853ab12073e1008961b421e269d76ed97545de3edd21097bc4046abb0f98cb62d166f8a1911f85a3b469433ee483ecd182eeee72312454e561bd13822cdfc32a6e2ab35b0a85f648942dfe372f63f1d0549d39e5f03ba5eb9abddb70be70e3865795ecc6fc047bf0c256712fbfc8cbce39a3b3ab6641bcc741ed4e36e5fb60650bc8e387fbe6b57546b7cc359b41c2d6e6a4b442a44ae97f4db79a2b064e16394b6d8bfca90d9814b6ec56da8d508c1e169e80da550412dd52e62c5e06c515d050f3326f4fc58dd47dbc0bcca8e5890691f72f9dfd4ff63c2373db27c01cde5e8db7ea6cebe2b58005879fb12be8b4e6bda290823402c139d919832d9d953014' \
+cd CSC2547/datasets/RSNA_Pneumonia
+curl 'https://storage.googleapis.com/kaggle-competitions-data/kaggle-v2/10338/862042/bundle/archive.zip?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1648272700&Signature=Jz7aHzeVnZoKQ95OhiV8JRs3Bjk5frJFObxxigpjM3aa0oxUJcwkDnWc3AJQ5eoAAjn0DAJhoNiUZPhxnzkyVTY7qNoi0nXavORbnqz4pRVC8Ptb09IY%2FCNXt1c%2FlHMGJY8ODC6t6G5AaYSAC1%2BE93AJPODw9kkjajQpl1QXxGsZ6iCsmO0TA22Zxb6CwjaoiXVQ1xuiJ55E9Ukbg2dAK6QQCS1gi6cMHpqFyIp%2BvNDO1Aj3b%2Fqwzl3YCEYq%2FxMqJW%2BQcjNZ%2F0ond2uQMl6wagICb9Gdx3kB47BWOm5wsj13Q1NjAJSL77W2aHUx4jzNTBZdX3ceAwQL1ok%2FDStZWw%3D%3D&response-content-disposition=attachment%3B+filename%3Drsna-pneumonia-detection-challenge.zip' \
   -H 'authority: storage.googleapis.com' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36' \
@@ -19,9 +22,9 @@ curl 'https://storage.googleapis.com/kaggle-data-sets/17810/23812/bundle/archive
   -H 'referer: https://www.kaggle.com/' \
   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
   --compressed -o data.zip
-unzip data.zip -d RNSA_Pneumonia
+unzip data.zip -d RSNA_Pneumonia
 rm data.zip
 
-cd ../
+cd ..
 
 
