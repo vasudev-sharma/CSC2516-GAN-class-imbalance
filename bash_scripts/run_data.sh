e3aee751dec1e2ca276646189efbd12cc2c17c65
vasudev-sharma <sharmavasudev1998@gmail.com>
2022-03-26 18:08:34 +0000

    Update download script

--- bash_scripts/run_data.sh
+++ bash_scripts/run_data.sh
@@ -5,9 +5,10 @@ git clone https://github.com/ieee8023/covid-chestxray-dataset.git
 mv covid-chestxray-dataset/* . & rm covid-chestxray-dataset
 cd ..
 
-
-curl 'https://storage.googleapis.com/kaggle-data-sets/17810/23812/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20220319%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20220319T215442Z&X-Goog-Expires=259199&X-Goog-SignedHeaders=host&X-Goog-Signature=853ab12073e1008961b421e269d76ed97545de3edd21097bc4046abb0f98cb62d166f8a1911f85a3b469433ee483ecd182eeee72312454e561bd13822cdfc32a6e2ab35b0a85f648942dfe372f63f1d0549d39e5f03ba5eb9abddb70be70e3865795ecc6fc047bf0c256712fbfc8cbce39a3b3ab6641bcc741ed4e36e5fb60650bc8e387fbe6b57546b7cc359b41c2d6e6a4b442a44ae97f4db79a2b064e16394b6d8bfca90d9814b6ec56da8d508c1e169e80da550412dd52e62c5e06c515d050f3326f4fc58dd47dbc0bcca8e5890691f72f9dfd4ff63c2373db27c01cde5e8db7ea6cebe2b58005879fb12be8b4e6bda290823402c139d919832d9d953014' \
+# Download RSNA dataset
+curl 'https://storage.googleapis.com/kaggle-competitions-data/kaggle-v2/10338/862042/bundle/archive.zip?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1648341989&Signature=MIKaXDiRHbnCPdirs%2FW7VxAY53MDxEi2NNMFB72EKMtKARq%2BZ8jNwX%2FYVR3P%2BqlWiYA7tmmrLFHvCWey2tPT6WnUGDwo6n2FbRF7hRmGEnsiiP6jKtFq%2FFBz2mdHKSVE%2FGlvaSBV7%2FufHXphfXB1Ol%2Ff75R0HcN%2BAw58otztJD5k6KazmVE63ih6BnxjEt%2F1HuZEx3ZzjHhkqBu4adqQA6J3wsLjNc8oumJUKuZUE95fKl89SeNamZFxKHzj02IRMDF1osjCrTz95xHhqlPGeHt%2BmYItcAs%2BstQA8qvl9%2BhZtBfT2cQNoB0PgItrsbdIiPEAoAtOjSIqmGSYYNooLQ%3D%3D&response-content-disposition=attachment%3B+filename%3Drsna-pneumonia-detection-challenge.zip' \
   -H 'authority: storage.googleapis.com' \
+  -H 'cache-control: max-age=0' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36' \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
@@ -26,4 +27,4 @@ rm data.zip
 
 cd ..
 
-
+# Install python dependecies
