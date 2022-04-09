5dcdd89fd01a9fb68a794bf52ba7f8c6a705f4c2
vasudev-sharma <sharmavasudev1998@gmail.com>
2022-04-09 20:46:35 +0000

    Refactor main

--- main.py
+++ main.py
@@ -41,27 +41,23 @@ from scripts.training import load_data, get_model, training, testing
 
 
 
-use_gpu = torch.cuda.is_available()
-print("Using GPU: {}".format(use_gpu))
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-if torch.cuda.is_available():
-    print("Using the GPU!")
-else:
-    print("WARNING: Could not find GPU! Using CPU only")
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--idx', type=int, required=True)
-parser.add_argument('--user', type=str, required=True)
-parser.add_argument('--with_gan', type=bool, required=True)
-parser.add_argument('--dataset_size', type=int, required=True)
-parser.add_argument('--skip_training', type=bool, required=False)
-parser.add_argument('--dataset', help = 'RSNA, COVID, COVID-small, MNIST', type=str, required=False)
-parser.add_argument('--fraction', help='Enter the fraction of the training data', type=float, required=True)
-parser.add_argument('--batch_size', help="Batch Size", type=int, required=False, default=32)
-parser.add_argument('--lr', help="Learning Rate", type=float, required=False, default=1e-3)
-parser.add_argument('--epochs', help="Total Number of Epochs", type=int, required=False, default=30)
-parser.add_argument('--data_aug', help="Add data augmentation or not", type=bool, required=False, default=False)
-FLAGS = parser.parse_args()
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--idx', type=int, required=True)
+    parser.add_argument('--user', type=str, required=True)
+    parser.add_argument('--with_gan', type=bool, required=True)
+    parser.add_argument('--dataset_size', type=int, required=True)
+    parser.add_argument('--skip_training', type=bool, required=False)
+    parser.add_argument('--dataset', help = 'RSNA, COVID, COVID-small, MNIST', type=str, required=False)
+    parser.add_argument('--fraction', help='Enter the fraction of the training data', type=float, required=True)
+    parser.add_argument('--batch_size', help="Batch Size", type=int, required=False, default=32)
+    parser.add_argument('--lr', help="Learning Rate", type=float, required=False, default=1e-3)
+    parser.add_argument('--epochs', help="Total Number of Epochs", type=int, required=False, default=30)
+    parser.add_argument('--data_aug', help="Add data augmentation or not", type=bool, required=False, default=False)
+    FLAGS = parser.parse_args()
+    return FLAGS
+
 
 
 # ADD SEED for consistent result
@@ -73,29 +69,6 @@ random.seed(SEED)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.deterministic = False
 os.environ['PYTHONHASHSEED'] = str(SEED)
-
-
-idx = FLAGS.idx
-user = FLAGS.user
-with_gan = FLAGS.with_gan
-skip_training = FLAGS.skip_training
-dataset_size = FLAGS.dataset_size
-frac = FLAGS.fraction
-
-config = {
-    'idx': idx,
-'user': user,
-'with_gan': with_gan,
-'skip_training': skip_training,
-'dataset_size': dataset_size,
-'frac': frac,
-'dataset': FLAGS.dataset,
-'data_aug': FLAGS.data_aug
-}
-
-wandb.init(entity='vs74', project='CSC2516')
-wandb.config.update(config)
-
 
 def get_paths(FLAGS, user):
     if user == "shobhita":
@@ -123,87 +96,121 @@ def get_paths(FLAGS, user):
         raise Exception("Invalid user")
     return data_path,output_path,model_path
 
-data_path, output_path, model_path = get_paths(FLAGS, user)
 
-model_name = "densenet_{}_{}".format(idx, with_gan)
+if __name__ == "__main__":
+    use_gpu = torch.cuda.is_available()
+    print("Using GPU: {}".format(use_gpu))
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        print("Using the GPU!")
+    else:
+        print("WARNING: Could not find GPU! Using CPU only")
 
-print("OUTPUT PATH: {}".format(output_path))
-print("MODEL PATH: {}".format(model_path + model_name))
-sys.stdout.flush()
-
-dataset_full_train, dataset_test = load_data(data_path, dataset_size, with_gan, FLAGS.data_aug, FLAGS.dataset)
-
-params = {}
-model_id = 1
-# for batch_size in [16, 32, 64]:
-#     for lr in [1e-2, 0.005, 0.001]:
-#         for optimizer in ["momentum", "adam"]:
-#             params[model_id] = {
-#                 "batch_size": batch_size,
-#                 "lr": lr,
-#                 "optimizer": optimizer
-#             }
-#             model_id += 1
-
-for batch_size in [16, 32]:
-    for lr in [0.001]:
-        for optimizer in ["adam"]:
-            params[model_id] = {
-                "batch_size": batch_size,
-                "lr": lr,
-                "optimizer": optimizer
-            }
-            model_id += 1
-
-if idx == 0:
-    model_params = {}
-    batch_size = 32
-    lr = 0.001
-    optimizer = "adam"
-else:
-    model_params = params[idx]
-    batch_size = model_params["batch_size"]
-    lr = model_params["lr"]
-    optimizer = model_params["optimizer"]
-
-split = 0.05
-val_length = int(split * len(dataset_full_train))
-dataset_val, dataset_train = random_split(dataset_full_train, [val_length, len(dataset_full_train) - val_length])
-
-ds_frac_train_len = int(frac * len(dataset_train))
-ds_frac_untrain_len = len(dataset_train) - ds_frac_train_len
-dataset_train, dataset_untrain = random_split(dataset_train, [ds_frac_train_len, ds_frac_untrain_len])
-print()
-print('Len of the training dataset is', len(dataset_train))
-
-dataLoaderTrain = DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True,  num_workers=2, pin_memory=True)
-dataLoaderVal = DataLoader(dataset=dataset_val, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
-dataLoaderTest = DataLoader(dataset=dataset_test, batch_size=batch_size, num_workers=2, pin_memory=True)
-
-print("Batch size: {}".format(batch_size))
-
-print("Learning rate: {}".format(lr))
-print("Optimizer: {}".format(optimizer))
-print("WITH GAN DATA: {}".format(with_gan))
-print("Train dataset size: {}".format(len(dataset_train)))
+    FLAGS = get_parser()
 
 
-# Log hyperparameters
-wandb.config.batch_size = batch_size
-wandb.config.lr = lr
-wandb.config.optimizer = optimizer
-wandb.config.epochs = FLAGS.epochs
+    idx = FLAGS.idx
+    user = FLAGS.user
+    with_gan = FLAGS.with_gan
+    skip_training = FLAGS.skip_training
+    dataset_size = FLAGS.dataset_size
+    frac = FLAGS.fraction
 
-if FLAGS.dataset == 'COVID':
-    num_classes = 25
-elif FLAGS.dataset == "RSNA":
-    num_classes = 2
-elif FLAGS.dataset == "COVID-small":
-    num_classes = 3
-else:
-    raise Exception("Invalid Option entered")
+    config = {
+        'idx': idx,
+    'user': user,
+    'with_gan': with_gan,
+    'skip_training': skip_training,
+    'dataset_size': dataset_size,
+    'frac': frac,
+    'dataset': FLAGS.dataset,
+    'data_aug': FLAGS.data_aug
+    }
 
-model = get_model(num_classes=num_classes)
+    wandb.init(entity='vs74', project='CSC2516')
+    wandb.config.update(config)
+
+    data_path, output_path, model_path = get_paths(FLAGS, user)
+
+    model_name = "densenet_{}_{}".format(idx, with_gan)
+
+    print("OUTPUT PATH: {}".format(output_path))
+    print("MODEL PATH: {}".format(model_path + model_name))
+    sys.stdout.flush()
+
+    dataset_full_train, dataset_test = load_data(data_path, dataset_size, with_gan, FLAGS.data_aug, FLAGS.dataset)
+
+    params = {}
+    model_id = 1
+    # for batch_size in [16, 32, 64]:
+    #     for lr in [1e-2, 0.005, 0.001]:
+    #         for optimizer in ["momentum", "adam"]:
+    #             params[model_id] = {
+    #                 "batch_size": batch_size,
+    #                 "lr": lr,
+    #                 "optimizer": optimizer
+    #             }
+    #             model_id += 1
+
+    for batch_size in [16, 32]:
+        for lr in [0.001]:
+            for optimizer in ["adam"]:
+                params[model_id] = {
+                    "batch_size": batch_size,
+                    "lr": lr,
+                    "optimizer": optimizer
+                }
+                model_id += 1
+
+    if idx == 0:
+        model_params = {}
+        batch_size = 32
+        lr = 0.001
+        optimizer = "adam"
+    else:
+        model_params = params[idx]
+        batch_size = model_params["batch_size"]
+        lr = model_params["lr"]
+        optimizer = model_params["optimizer"]
+
+    split = 0.05
+    val_length = int(split * len(dataset_full_train))
+    dataset_val, dataset_train = random_split(dataset_full_train, [val_length, len(dataset_full_train) - val_length])
+
+    ds_frac_train_len = int(frac * len(dataset_train))
+    ds_frac_untrain_len = len(dataset_train) - ds_frac_train_len
+    dataset_train, dataset_untrain = random_split(dataset_train, [ds_frac_train_len, ds_frac_untrain_len])
+    print()
+    print('Len of the training dataset is', len(dataset_train))
+
+    dataLoaderTrain = DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True,  num_workers=2, pin_memory=True)
+    dataLoaderVal = DataLoader(dataset=dataset_val, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+    dataLoaderTest = DataLoader(dataset=dataset_test, batch_size=batch_size, num_workers=2, pin_memory=True)
+
+    print("Batch size: {}".format(batch_size))
+
+    print("Learning rate: {}".format(lr))
+    print("Optimizer: {}".format(optimizer))
+    print("WITH GAN DATA: {}".format(with_gan))
+    print("Train dataset size: {}".format(len(dataset_train)))
+
+
+    # Log hyperparameters
+    wandb.config.batch_size = batch_size
+    wandb.config.lr = lr
+    wandb.config.optimizer = optimizer
+    wandb.config.epochs = FLAGS.epochs
+
+    if FLAGS.dataset == 'COVID':
+        num_classes = 25
+    elif FLAGS.dataset == "RSNA":
+        num_classes = 2
+    elif FLAGS.dataset == "COVID-small":
+        num_classes = 3
+    else:
+        raise Exception("Invalid Option entered")
+
+    model = get_model(num_classes=num_classes)
 
 if not skip_training:
     print("TRAINING")
